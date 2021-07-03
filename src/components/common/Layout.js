0abfbe061b4aa75160ea8ff2c17dd7bec832ac9b
nahornyy <ihor.nahornyy@gmail.com>
2021-07-03 21:11:57 +0300

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -9,12 +9,7 @@ import config from '../../utils/siteConfig'
 
 // Styles
 import '../../styles/app.css'
-<!-- UIkit CSS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.7.0/dist/css/uikit.min.css" />
 
-<!-- UIkit JS -->
-<script src="https://cdn.jsdelivr.net/npm/uikit@3.7.0/dist/js/uikit.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/uikit@3.7.0/dist/js/uikit-icons.min.js"></script>
 /**
 * Main layout component
 *
